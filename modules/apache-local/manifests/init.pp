e31477914cb50085e964de23361baf57d5e86e33
marsbard <martin@ciderpunk.com>
2014-09-17 19:21:50 +0200

    apache/mailman integration

--- modules/apache-local/manifests/init.pp
+++ modules/apache-local/manifests/init.pp
@@ -2,8 +2,8 @@ class apache-local {
 
     # https://forge.puppetlabs.com/puppetlabs/apache
     class { 'apache':
-      default_mods        => false,
-        default_confd_files => false,
+      #default_mods        => false,
+       # default_confd_files => false,
     }
 
     apache::vhost { "lists.orderofthebee.org":
