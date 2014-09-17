cd0e9b9701378e32ebb58a1e0d57533285910812
marsbard <martin@ciderpunk.com>
2014-09-17 17:27:42 +0200

    adding apache

--- apply-puppet.sh
+++ apply-puppet.sh
@@ -2,6 +2,20 @@
 
 cd "`dirname $0`"
 
+if [ ! -f "IS_REMOTE" ]
+then
+    echo 
+    echo "Be careful applying this to your local machine, use vagrant instead"
+    echo "or else your apache config (if you have one) will be destroyed."
+    echo
+    echo "If this really is the remote machine, then issue the following command:"
+    echo
+    echo "  touch `pwd`/IS_REMOTE"
+    echo
+    exit
+fi
+
+
 ./install-puppet-modules.sh
 
 sudo puppet apply -v -d --modulepath="./modules:./extmodules" manifests/default.pp
--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -5,3 +5,4 @@ TGTDIR=extmodules
 cd "`dirname $0`"
 
 puppet module install --force thias-mailman --target-dir $TGTDIR
+puppet module install --force puppetlabs-apache --target-dir $TGTDIR
